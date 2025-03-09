b20247462b10cd0d028bd97c282b616f45367e9b
yurii-bezus <bylchenok@gmail.com>
2025-03-09 21:03:03 +0200

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return message;
+  const totalPrice = price + deliveryFee;
+  const message = `Shipping to ${country} will cost ${totalPrice} credits`;
+  return message;
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
